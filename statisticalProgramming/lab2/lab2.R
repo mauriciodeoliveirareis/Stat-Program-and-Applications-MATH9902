be4c6d256d780a6b0ced471f81015b545a4d1e74
mauricio.reis <mauricio.reis@workday.com>
2021-10-07 10:01:14 +0100

    added Assignnment 1 and changed a few things on lab2

--- statisticalProgramming/lab2/lab2.R
+++ statisticalProgramming/lab2/lab2.R
@@ -1,13 +1,11 @@
-Urn_draws <- function(ndraws = 1, nOfBallsOnUrn = 100, invalidNumberOfDrawsMessage = 'ndraws cannot be > 100!') {
+Urn_draws <- function(ndraws = 1, nOfBallsOnUrn = 100, invalidNumberOfDrawsMessage = 'ndraws cannot be > nOfBallsOnUrn!') {
   
   if (ndraws > nOfBallsOnUrn) stop(invalidNumberOfDrawsMessage)
   
   balls <- sample(1:nOfBallsOnUrn, size = ndraws, replace = FALSE)
   for (ball in balls) {
-    if(ball < 10) print(ball)
+    if(ball < 10) print(paste(ball, '< 10'))
   }
-  print("Balls sorted:")
-  balls <- sort(balls)
 
   mean <- mean(balls)
   
@@ -17,10 +15,13 @@ Urn_draws <- function(ndraws = 1, nOfBallsOnUrn = 100, invalidNumberOfDrawsMessa
   
   even_prop <- sum((balls %% 2) == 0) / ndraws
   
-  list(mean = mean,
+  sortedBalls <- sort(balls)
+  
+  list(meanzz = mean,
        min = min,
        max = max,
-       even_prop = even_prop)
+       even_prop = even_prop,
+       sortedBalls = sortedBalls)
 }
 
 set.seed(7)
