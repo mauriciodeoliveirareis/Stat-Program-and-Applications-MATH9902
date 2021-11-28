168843612d6bb785902c9dcfd6c5b0625f3bc9d8
mauricio.reis <mauricio.reis@workday.com>
2021-11-28 08:01:33 -0500

    fixed minor issues on lab 8

--- statisticalProgramming/lab8/lab8.R
+++ statisticalProgramming/lab8/lab8.R
@@ -4,14 +4,16 @@ explainRandomSample <- function(n, mu, sigmaSq, x1) {
   sample <- rnorm(n,mu,sigma)
   xbar <- mean(sample)
   s <- sd(sample)
-  se <- s/n
+  se <- s/sqrt(n)
   x1CdfSample <- pnorm(x1, xbar, s)
   x1CdfPopulation <- pnorm(x1, mu, sigma)
+  F_x1 <- sum(sample < x1) / n
   list(xbar = xbar,
        s = s,
        se = se, 
        x1CdfSample = x1CdfSample,
-       x1CdfPopulation = x1CdfPopulation)
+       x1CdfPopulation = x1CdfPopulation,
+       F_x1 = F_x1)
 }
 set.seed(1) 
 #explainRandomSample(1,1,1,1) #n must be between 100 and 10,000
